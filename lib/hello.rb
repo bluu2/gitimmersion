cd63056972d1fc32fc04b2229205a083fa1c05a1
Brandon <bluu2@uncc.edu>
2014-04-09 12:40:29 -0400

    making some fixes pt2

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
-# Default is "World"
-# Author: Jim Weirich(jim@somewhere.com)
+require 'greeter'
+
+# Default is World
 name = ARGV.first || "World"
 
-puts "Hello, #{ARGV.first}!"
+greeter = Greeter.new(name)
+puts greeter.greet
